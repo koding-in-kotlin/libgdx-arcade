dc4a726de4da5191ad33b523ac0c13770b59e148
asm0dey <pavel.finkelshtein@gmail.com>
2023-05-05 12:29:29 +0200

    HERE IS THE MAP

--- core/src/main/kotlin/com/github/kodinginkotlin/Main.kt
+++ core/src/main/kotlin/com/github/kodinginkotlin/Main.kt
@@ -1,19 +1,15 @@
 package com.github.kodinginkotlin
 
-import com.badlogic.gdx.Gdx.*
+import com.badlogic.gdx.Gdx.graphics
+import com.badlogic.gdx.Gdx.input
 import com.badlogic.gdx.Input
-import com.badlogic.gdx.graphics.Color
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.Texture.TextureFilter.Linear
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer
-import com.badlogic.gdx.math.Vector3
-import com.badlogic.gdx.utils.viewport.FillViewport
+import com.badlogic.gdx.maps.tiled.TmxMapLoader
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
 import ktx.app.KtxGame
 import ktx.app.KtxScreen
 import ktx.app.clearScreen
 import ktx.assets.disposeSafely
-import ktx.assets.toInternalFile
 import ktx.async.KtxAsync
 import ktx.graphics.use
 
@@ -27,22 +23,28 @@ class Main : KtxGame<KtxScreen>() {
 }
 
 class FirstScreen : KtxScreen {
-    private val image = Texture("logo.png".toInternalFile(), true).apply { setFilter(Linear, Linear) }
+    //    private val image = Texture("logo.png".toInternalFile(), true).apply { setFilter(Linear, Linear) }
     private val batch = SpriteBatch()
-    private val renderer = ShapeRenderer()
-    private val camera = ShakyCamera(graphics.width, graphics.height).apply{
-        position.x = graphics.width.toFloat() / 2
-        position.y = graphics.height.toFloat() / 2
+
+    //    private val renderer = ShapeRenderer()
+    private val camera = ShakyCamera(graphics.width, graphics.height).apply {
+        position.x = 256f
+        position.y = 160f
+        zoom = .5f
         update()
     }
     private var x = 300f
     private var y = 300f
     private var flipflop = 1.0f
+    val map = TmxMapLoader().load("maps/arcade/tiled/Level_0.tmx")
+    val renderer = OrthogonalTiledMapRenderer(map)
 
     override fun render(delta: Float) {
         clearScreen(red = 0.7f, green = 1.0f, blue = 1.0f)
+        renderer.setView(camera)
         batch.use(camera) {
-            it.draw(image, 100f, 160f)
+//            it.draw(image, 100f, 160f)
+            renderer.render()
         }
 
         // read kb, adjust y
@@ -59,24 +61,27 @@ class FirstScreen : KtxScreen {
         }
 
         // Projection matrix will be copied from the camera:
-        renderer.use(ShapeRenderer.ShapeType.Filled) {
-            // Operate on shapeRenderer instance
-            it.color = Color.PINK
-            x += delta * 500 * flipflop
-            if ((x > graphics.width - radius) or (x < radius)) {
-                flipflop *= -1
-                camera.shake()
-            }
-            camera.update()
-            camera.position.lerp(Vector3((graphics.width).toFloat()/2, (graphics.height).toFloat()/2, 0f), .2f)
-
-            it.circle(x, y, radius)
-        }
+//        renderer.use(ShapeRenderer.ShapeType.Filled) {
+//            // Operate on shapeRenderer instance
+//            it.color = Color.PINK
+//            x += delta * 500 * flipflop
+//            if ((x > graphics.width - radius) or (x < radius)) {
+//                flipflop *= -1
+//                camera.shake()
+//            }
+//            camera.update()
+//            camera.position.lerp(Vector3((graphics.width).toFloat()/2, (graphics.height).toFloat()/2, 0f), .2f)
+//
+//            it.circle(x, y, radius)
+//        }
 
     }
 
     override fun dispose() {
-        image.disposeSafely()
+//        image.disposeSafely()
         batch.disposeSafely()
+//        renderer.disposeSafely()
+        map.disposeSafely()
+        renderer.disposeSafely()
     }
 }
