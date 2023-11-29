bebff194c025ab378c9e5a53203bf8bc4ea24e4f
asm0dey <pavel.finkelshtein@gmail.com>
2023-11-29 15:37:43 +0100

    Updates TeaVMBuilder to fix incompatibility

--- teavm/src/main/kotlin/com/github/kodinginkotlin/teavm/TeaVMBuilder.kt
+++ teavm/src/main/kotlin/com/github/kodinginkotlin/teavm/TeaVMBuilder.kt
@@ -1,15 +1,15 @@
 package com.github.kodinginkotlin.teavm
 
 import java.io.File
-import com.github.xpenatan.gdx.backends.teavm.TeaBuildConfiguration
-import com.github.xpenatan.gdx.backends.teavm.TeaBuilder
-import com.github.xpenatan.gdx.backends.teavm.plugins.TeaReflectionSupplier
+import com.github.xpenatan.gdx.backends.teavm.config.TeaBuildConfiguration
+import com.github.xpenatan.gdx.backends.teavm.config.TeaBuilder
 import com.github.xpenatan.gdx.backends.web.gen.SkipClass
 
 /** Builds the TeaVM/HTML application. */
 @SkipClass
 object TeaVMBuilder {
-    @JvmStatic fun main(arguments: Array<String>) {
+    @JvmStatic
+    fun main(arguments: Array<String>) {
         val teaBuildConfiguration = TeaBuildConfiguration().apply {
             assetsPath.add(File("../assets"))
             webappPath = File("build/dist").canonicalPath
